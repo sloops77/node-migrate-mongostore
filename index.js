93711c496d85c157783d1aa04a047a07ae1c8f52
Andres Olave <andres.olave@thinkingbytes.net>
2016-08-01 13:32:39 +0300

    add support for locks

--- index.js
+++ index.js
@@ -6,8 +6,15 @@ var MongoClient = require('mongodb');
 module.exports = MongoStore;
 
 function MongoStore(options) {
-    this.mongoUrl = options.mongoUrl || process.env.MONGO_URL || 'mongodb://localhost:27017/admin';
-    this.collectionName = options.mongoCollection || 'migrations';
+  this.mongoUrl = options.mongoUrl || process.env.MONGO_URL || 'mongodb://localhost:27017/admin';
+  this.collectionName = options.mongoCollection || 'migrations';
+
+  this.getLockCollection(function (err, c) {
+    if (err)
+      return;
+
+    c.createIndex({lockName: 1}, {unique: true});
+  });
 }
 
 MongoStore.cliHandler = {
@@ -25,9 +32,17 @@ MongoStore.cliHandler = {
     }
 };
 
+MongoStore.prototype.getLockCollection = function (callback) {
+  this._getCollection(this.collectionName + '-lock', callback);
+};
+
 MongoStore.prototype.getCollection = function (callback) {
+  this._getCollection(this.collectionName, callback);
+};
+
+MongoStore.prototype._getCollection = function (collectionName, callback) {
     if (this.db) {
-        return callback(null, this.db.collection(this.collectionName));
+        return callback(null, this.db.collection(collectionName));
     }
     var self = this;
     MongoClient.connect(self.mongoUrl, {}, function (err, db) {
@@ -35,33 +50,76 @@ MongoStore.prototype.getCollection = function (callback) {
             return callback(err);
 
         self.db = db;
-        callback(null, db.collection(self.collectionName));
+        callback(null, db.collection(collectionName));
     });
 };
 
 MongoStore.prototype.load = function (callback) {
-    return this.getCollection(function (err, c) {
+    var self = this;
+    this.lock(function (err) {
         if (err)
-            callback(err);
+            return callback(err);
 
-        c.find().sort({timestamp: 1}).toArray(callback);
+        return self.getCollection(function (err, c) {
+            if (err)
+                callback(err);
+
+            c.find({title: {$exists: true}}).sort({timestamp: 1}).toArray(callback);
+        });
     });
 };
 
 MongoStore.prototype.save = function (migration, callback) {
     return this.getCollection(function (err, c) {
         if (err)
-            callback(err);
+            return callback(err);
 
         c.insert(migration, {forceServerObjectId: true}, callback);
     });
 };
 
-MongoStore.prototype.reset = function (callback) {
-    return this.getCollection(function (err, c) {
-        if (err)
-            callback(err);
-
-        c.drop(callback);
+MongoStore.prototype.lock = function (callback) {
+    this.getLockCollection(function (err, c) {
+      c.findAndModify(
+        {lockName: '_node_migrate_lock_', lock: {$ne: true}},
+        [],
+        {$set: {timestamp: new Date(), lock: true}},
+        {upsert: true, new: true},
+        function (err, retVal) {
+            if (err) {
+              callback(new Error('Blocked: Migration Table Already Locked'));
+            } else {
+              console.log('locked');
+                callback(null, retVal);
+            }
+        });
     });
+};
+
+MongoStore.prototype.unlock = function (callback) {
+  this.getLockCollection(function (err, c) {
+    c.findAndModify(
+      {lockName: '_node_migrate_lock_'},
+      [],
+      {$set: {lock: false}},
+      {},
+      function (err, retVal) {
+        console.log('unlocked');
+        callback(err, retVal);
+      });
+  });
+};
+
+MongoStore.prototype.reset = function (callback) {
+  var self = this
+  self.getLockCollection(function (err, c) {
+    c.drop();
+
+    return self.getCollection(function (err, c) {
+      if (err)
+        callback(err);
+
+      c.drop(callback);
+    });
+  });
 };
